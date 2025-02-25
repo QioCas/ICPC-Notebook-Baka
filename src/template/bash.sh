d3578e5bacc2cd752c9cec3b617189a762c77148
QioCas <106325835+QioCas@users.noreply.github.com>
2025-02-26 01:00:44 +0700

    Add stress test and pragma

---
+++ src/template/bash.sh
@@ -0,0 +1,38 @@
+memory=524288  # 512MB
+stack_size=524288  # 512MB
+
+ulimit -v "${memory}"
+ulimit -s "${stack_size}"
+
+g++ main.cpp -DSTRESS -O2 -std=c++17 -Wno-unused-result -Wshadow -Wall -o main
+g++ brute.cpp -DSTRESS -O2 -std=c++17 -Wno-unused-result -Wshadow -Wall -o brute
+g++ gen.cpp -DSTRESS -O2 -std=c++17 -Wno-unused-result -o gen
+
+
+for((i = 1; i <= 1000; ++i)); do {
+
+    ./gen > test.in 
+    ./main  < test.in > test.out
+    ./brute < test.in > test.ans
+
+    # External grading program:
+    # ~/Cp/Stress/checker > /dev/null || {
+    # General:
+    diff -Z test.out test.ans > /dev/null || {
+        echo "WA on the following test:"
+        cat test.in
+        echo "=================="
+        echo "Your answer is:"
+        cat test.out
+        echo "=================="
+        echo "Correct answer is:"
+        cat test.ans
+        break
+    }
+    echo "Passed test: "  $i
+}
+done
+
+rm main
+rm gen
+rm brute
---
+++ src/template/pragma.cpp
@@ -0,0 +1,2 @@
+#pragma GCC optimize("unroll-loops,02,fast-math, no-stack-protector")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
